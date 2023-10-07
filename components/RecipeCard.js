1bee4d4e583930a22b25bbf8eb8c6f7b8b37d3a1
Devin <devhunter16@gmail.com>
2023-10-07 13:45:30 -0700

    Added RecipeCard component and styling and linked DrinksList's styles to the DrinksList component appropriately.

---
+++ components/RecipeCard.js
@@ -0,0 +1,17 @@
+import styles from "./styles/RecipeCard.module.css";
+
+function RecipeCard({ name, image, recipe }) {
+    return (
+        <div className={styles.container}>
+            <div className={styles.cardContainer}>
+                <h3 className={styles.cocktailName}>{name}</h3>
+                <img src={image} className={styles.cardImage} />
+                <div id={styles.instructionsBox}>
+                    <p className={styles.recipeInstructions}>{recipe}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RecipeCard;
