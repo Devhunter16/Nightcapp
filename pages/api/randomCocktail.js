b92d68f6d1346c1a101391c9e756a4d4411ed723
Devin <devhunter16@gmail.com>
2023-10-22 09:59:53 -0700

    Added Alert component and styling file to be used to set alerts for various user actions.

--- pages/api/randomCocktail.js
+++ pages/api/randomCocktail.js
@@ -1,28 +1,16 @@
 import axios from "axios";
 
 async function randomCocktail() {
-    try {
-        const response = await axios.get(
-            "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-        );
-        const recipe = response.data.drinks[0];
-        console.log(recipe);
-        return (await recipe);
-    } catch (err) {
-        console.error(err);
-    };
+  try {
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+    const recipe = response.data.drinks[0];
+    console.log(recipe);
+    return (await recipe);
+  } catch (err) {
+    console.error(err);
+  };
 };
 
 export default randomCocktail;
-
-/*
-try {
-  const response = axios({
-    method: "get",
-    url: `http://localhost:3001/cocktaildb/random_recipe/?recipe=${data}`,
-  });
-  return (await response).data;
-} catch (error) {
-  console.error(error);
-};
-*/
