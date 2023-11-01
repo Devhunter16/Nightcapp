357e1139d69987f10dd71c15b16e4f76c6c5d392
Devin <devhunter16@gmail.com>
2023-11-01 09:24:42 -0700

    Changed Navbar logic and styling so that the appropriate nav link would light up when you're on the appropriate page.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,22 @@
 import styles from "./Navbar.module.css";
 
+import { useRouter } from "next/router";
+
 import Link from "next/link";
 
 function Navigation() {
+    // Only using useRouter to make so that if the user is on the home screen "Home"
+    // in the Navbar is highlighted, and the same happens on the "About us" page
+    const router = useRouter();
+
     return (
         <div className={styles.nav}>
             <ul className={styles.navList}>
-                <li className={styles.navItem}>
+                <li className={`${styles.navItem} ${router.pathname === "/" ? styles.active : ""}`}>
                     <Link href="/">Home</Link>
                 </li>
-                <li className={styles.navItem}>
-                    <Link href="/about" legacyBehavior>
-                        About Us
-                    </Link>
+                <li className={`${styles.navItem} ${router.pathname === "/about" ? styles.active : ""}`}>
+                    <Link href="/about">About Us</Link>
                 </li>
             </ul>
         </div>
